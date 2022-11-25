65eb5229599d54ee48d40f7fd52ba6e4a733395f
jeevaann1999 <jeevaann1999@gmail.com>
2022-11-25 06:17:10 +0530

    book edit added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { BooksEntryComponent } from './books-entry/books-entry.component';
 import { ViewBooksComponent } from './view-books/view-books.component';
 import { BookSearchComponent } from './book-search/book-search.component';
 import { BookDeleteComponent } from './book-delete/book-delete.component';
+import { BookEditComponent } from './book-edit/book-edit.component';
 
 @NgModule({
   declarations: [
@@ -20,7 +21,8 @@ import { BookDeleteComponent } from './book-delete/book-delete.component';
     BooksEntryComponent,
     ViewBooksComponent,
     BookSearchComponent,
-    BookDeleteComponent
+    BookDeleteComponent,
+    BookEditComponent
   ],
   imports: [
     BrowserModule,
