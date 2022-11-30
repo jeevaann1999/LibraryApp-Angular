9794debf770ed03d89d70178ac345e50bb7205b5
jeevaann1999 <jeevaann1999@gmail.com>
2022-11-30 16:52:03 +0530

    authentication

--- src/app/admin-login/admin-login.component.ts
+++ src/app/admin-login/admin-login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-admin-login',
@@ -9,6 +10,8 @@ export class AdminLoginComponent {
 username=""
 password=""
 
+constructor(private route:Router){}
+
 readValues=()=>{
   let data:any={
     "username":this.username,
@@ -17,7 +20,7 @@ readValues=()=>{
   console.log(data)
 
   if (this.username=="admin" && this.password=="12345") {
-    alert("Valid credentials")
+    this.route.navigate(['/entry'])
   } else {
     alert("Invalid credentials")
   }
